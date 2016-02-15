5b14eaa3629f16085d130b799ecc855cf8b280b0
David Best <david.best@gsa.gov>
2016-02-15 16:48:40 -0500

    Initial commit.

---
+++ scripts/buildjson.py
@@ -0,0 +1,28 @@
+import dbm
+import csv
+import json
+from datetime import datetime
+
+def get_city(record):
+    return ' '.join(latlon[2:])
+
+db = dbm.open('./centroids.dbm', 'r')
+#open output json file
+file = open('georeaps.json', 'w')
+values = []
+with open('reapitems.csv') as csvfile:
+    reader = csv.DictReader(csvfile, ['parcel', 'street', 'eligible', 'paymentplan', 'lastyear', 'paymentwindow', 'class'])
+    for row in reader:
+        if row['eligible'] != 'Sold' and row['paymentplan'] == 'False' and row['paymentwindow'] == 'False' and row['lastyear'] < '2013' and row['class'] == 'R':
+            try:
+                latlon = db[row['parcel']].split()
+                values.append({'parcelid': row['parcel'], 'street': row['street'] + ', ' + get_city(latlon), 'lat': latlon[0], 'lon': latlon[1]})
+            except Exception as e:
+                print(e)
+                #if not, log it in a separate json file for analysis/reporting
+                print row['parcel']
+    file.write('var lastupdated = new Date("' + datetime.now().strftime("%B %d, %Y %H:%M:%S") + '");\n')
+    file.write('var points =')
+    file.write(json.dumps(values, indent=2))
+    file.write(';')
+    file.close()
